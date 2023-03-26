dfb8d9d0d8f31fd7b0ffe1eafc3d2d30a9c5a3e1
Ömer Yaltırık <40443652+OmerYALTIRIK@users.noreply.github.com>
2023-03-26 03:58:17 +0300

    Ürün fotoğrafları eklendi

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useState } from "react";
+import "./App.css";
+import { Container, SimpleGrid, List, ThemeIcon, Input } from "@mantine/core";
+import { IconCircleCheck, IconCircleDashed } from "@tabler/icons-react";
+import Card from "./components/Card";
+
+const storeItems = [
+  { name: "Fotoğraf Makinası", src: "camera", price: 20 },
+  { name: "Kulaklık", src: "headphone", price: 10 },
+  { name: "Oyun Konsolu", src: "joystick", price: 25 },
+  { name: "Retro Fotoğraf Makinası", src: "retro-cam", price: 25 },
+  { name: "Oyuncak Araba", src: "toy-car", price: 25 },
+  { name: "Kol Saati", src: "watch", price: 25 },
+];
 
 function App() {
+  let [basketItems, setBasketItems] = useState([]); //useState ile basketItems değişkeni oluşturduk ve bunun başlangıç değer
+  let [searchValue, setSearchValue] = useState(""); //useState ile search değişkeni oluşturduk ve bunun başlangıç değeri boş bir string
+  let filteredItems = basketItems.filter((item) => item.name.toLowerCase().indexOf(searchValue.toLowerCase()) >= 0);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <SimpleGrid cols={3} className="Store">
+        {storeItems.map(({ name, src }) => {
+          return <Card key={name} name={name} src={src} onAdd={() => setBasketItems([...basketItems, { name }])} />; //bir nesneyi map ile döndürürken unique key vermek zorundayız
+        })}
+      </SimpleGrid>
+      <Input.Wrapper label="Arama">
+        <Input onChange={(e) => setSearchValue(e.target.value)} />
+      </Input.Wrapper>
+      <List
+        className="List"
+        spacing="xs"
+        size="sm"
+        center
+        icon={
+          <ThemeIcon color="teal" size={24} radius="xl">
+            <IconCircleCheck size="1rem" />
+          </ThemeIcon>
+        }
+      >
+        {filteredItems.map(({ name }, index) => {
+          return <List.Item key={name + index}>{name}</List.Item>;
+        })}
+      </List>
+    </Container>
   );
 }
 
---
+++ src/components/Card.js
@@ -0,0 +1,28 @@
+import { Card, Image, Text, Badge, Button, Group } from "@mantine/core";
+
+const Cardcomponent = ({ name, src, onAdd }) => {
+  return (
+    <Card shadow="sm" padding="lg" radius="md" withBorder>
+      <Card.Section>
+        <Image src={`/assets/image/${src}.jpg`} height={160} alt="Norway" />
+      </Card.Section>
+
+      <Group position="apart" mt="md" mb="xs">
+        <Text weight={500}>{name}</Text>
+        <Badge color="pink" variant="light">
+          On Sale
+        </Badge>
+      </Group>
+
+      <Text size="sm" color="dimmed">
+        With Fjord Tours you can explore more of the magical fjord landscapes with tours and activities on and around the fjords of Norway
+      </Text>
+
+      <Button variant="light" color="blue" fullWidth mt="md" radius="md" onClick={onAdd}>
+        Ekle
+      </Button>
+    </Card>
+  );
+};
+
+export default Cardcomponent;
